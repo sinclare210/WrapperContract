49b01c814d1ca81e5ab11511d0a69cbb50b2ff38
sinclare210 <adebayooluwasegun2004@gmail.com>
2025-05-21 21:08:25 +0100

    Deployed

--- script/Wrapper.s.sol
+++ script/Wrapper.s.sol
@@ -1,2 +1,20 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.27;
+pragma solidity 0.8.28;
+
+import {Script, console} from "forge-std/Script.sol";
+import {Wrapper} from "../src/Wrapper.sol";
+
+//0x6395b37b7f966af183Af513FC0B956d2f288B1B6 token
+//0xb674d916813f39Df0d716B06498bBca1bF482186 wrapped
+contract WrapperScript is Script{
+    Wrapper public wrapper;
+
+    function setUp() external {}
+
+    function run() external {
+        vm.startBroadcast();
+        wrapper = new Wrapper(0x6395b37b7f966af183Af513FC0B956d2f288B1B6);
+        vm.stopBroadcast();
+    }
+
+}
---
+++ src/Sinclair.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity 0.8.28;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Sinclair is ERC20 {
+    constructor(address recipient) ERC20("Sinclair", "SINCS") {
+        _mint(recipient, 100000000000 * 10 ** decimals());
+    }
+
+    
+}
