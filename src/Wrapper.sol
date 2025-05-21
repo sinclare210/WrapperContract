0587a5e0ca37a9edf3a9e66b9e574ce48c8778a6
sinclare210 <adebayooluwasegun2004@gmail.com>
2025-05-21 05:35:02 +0100

    Nice

--- src/Wrapper.sol
+++ src/Wrapper.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.27;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 
