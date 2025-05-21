3622dc5e0bece0edb991755e7b17d31fe308dd6f
sinclare210 <adebayooluwasegun2004@gmail.com>
2025-05-21 19:25:16 +0100

    Done

--- src/Wrapper.sol
+++ src/Wrapper.sol
@@ -6,34 +6,33 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {Sinclair} from "./ERC20.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
-
 contract Wrapper is ERC20 {
-
     error ZeroNotAllowed();
-    
 
     using SafeERC20 for IERC20;
 
     IERC20 public immutable wrappedToken;
 
-    constructor(address _wrappedToken)
-   ERC20("Wrapped Sinclair", "wTKN"){
+    event Deposited(address indexed user, uint256 amount);
+    event Withdrawn(address indexed user, uint256 amount);
+
+    constructor(address _wrappedToken) ERC20("Wrapped Sinclair", "wTKN") {
         wrappedToken = IERC20(_wrappedToken);
-   }
+    }
 
-   function deposit (uint256 _amount) public {
-    if(_amount < 0) revert ZeroNotAllowed();
-    
-    wrappedToken.safeTransferFrom(msg.sender, address(this), _amount);
-    _mint(msg.sender, _amount);
-   }
+    function deposit(uint256 _amount) public {
+        if (_amount < 0) revert ZeroNotAllowed();
 
-   function withdraw (uint256 _amount) public {
-         if(_amount < 0) revert ZeroNotAllowed();
-         _burn(msg.sender, _amount);
-         wrappedToken.safeTransfer(msg.sender, _amount);
-   }
+        wrappedToken.safeTransferFrom(msg.sender, address(this), _amount);
+        _mint(msg.sender, _amount);
 
+        emit Deposited(msg.sender, _amount);
+    }
 
-
+    function withdraw(uint256 _amount) public {
+        if (_amount < 0) revert ZeroNotAllowed();
+        _burn(msg.sender, _amount);
+        wrappedToken.safeTransfer(msg.sender, _amount);
+        emit Withdrawn(msg.sender, _amount);
+    }
 }
