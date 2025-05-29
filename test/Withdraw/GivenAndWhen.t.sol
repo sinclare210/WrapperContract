bd4899f9956921a20dae76fe7a799c380ed48eb9
sinclare210 <adebayooluwasegun2004@gmail.com>
2025-05-29 01:54:13 +0100

    Done

--- test/Withdraw/GivenAndWhen.t.sol
+++ test/Withdraw/GivenAndWhen.t.sol
@@ -7,6 +7,10 @@ import {WrapperContract} from "src/Wrapper.sol";
 contract WhenAndGivenTestForWithdraw is WrapperTest {
     function test_Given_amountIs0() external {
         // it should revert with WithdrawAmountZero
+        vm.startPrank(sinc);
+        vm.expectRevert(WrapperContract.WithdrawAmountZero.selector);
+        wrapperContract.withdraw(WrapperContract.AssetType.ETH, 0);
+        vm.stopPrank();
     }
 
     modifier givenAssetIsETH() {
@@ -16,6 +20,11 @@ contract WhenAndGivenTestForWithdraw is WrapperTest {
 
     function test_Given_amountIsGreaterThanUserETHBalance() external givenAssetIsETH {
         // it should revert with InsufficientEthDeposit
+        vm.startPrank(sinc);
+        wrapperContract.deposit{value: 2 ether}(WrapperContract.AssetType.ETH, 0);
+        vm.expectRevert(WrapperContract.InsufficientEthDeposit.selector);
+        wrapperContract.withdraw(WrapperContract.AssetType.ETH, 3 ether);
+        vm.stopPrank();
     }
 
     function test_Given_amountLessThanOrEqualToUserETHBalance() external givenAssetIsETH {
@@ -24,6 +33,13 @@ contract WhenAndGivenTestForWithdraw is WrapperTest {
         // it should burn _amount of wrapped tokens from msg sender
         // it should transfer _amount ETH to msg sender
         // it should emit EthWithdrawal(msg sender, _amount)
+        vm.startPrank(sinc);
+        wrapperContract.deposit{value: 2 ether}(WrapperContract.AssetType.ETH, 0);
+        wrapperContract.withdraw(WrapperContract.AssetType.ETH, 1 ether);
+        assertEq(wrapperContract.balanceInETH(), 1 ether);
+        assertEq(wrapperContract.balanceInETHForUser(sinc), 1 ether);
+        emit WrapperContract.EthWithdrawal(sinc, 1 ether);
+        vm.stopPrank();
     }
 
     modifier givenAssetIsTOKEN() {
@@ -33,11 +49,25 @@ contract WhenAndGivenTestForWithdraw is WrapperTest {
 
     function test_GivenContractHasInsufficientTokenBalance() external givenAssetIsTOKEN {
         // it should revert with SafeERC20 error
+        vm.startPrank(sinc);
+
+        wrapperContract.deposit(WrapperContract.AssetType.TOKEN, 10000);
+        vm.expectRevert();
+        wrapperContract.withdraw(WrapperContract.AssetType.TOKEN, 10000000);
+        vm.stopPrank();
     }
 
     function test_GivenContractHasSufficientTokenBalance() external givenAssetIsTOKEN {
         // it should burn _amount of wrapped tokens from msg sender
         // it should transfer _amount tokens to msg sender
         // it should emit TokenWithdrawal(msg sender, _amount)
+        vm.startPrank(sinc);
+
+        wrapperContract.deposit(WrapperContract.AssetType.TOKEN, 10000);
+
+        wrapperContract.withdraw(WrapperContract.AssetType.TOKEN, 1000);
+        assertEq(wrapperContract.balanceOf(sinc), 10000 - 1000);
+        emit WrapperContract.TokenWithdrawal(sinc, 1000);
+        vm.stopPrank();
     }
 }
